e166530114b4e9918eb5918efe14972768dcbd4f
igoramf <igoraaf1@gmail.com>
2024-01-21 11:18:38 -0300

    style: added border to signIn and signUp boxes

--- front-end-falk/app/(pages)/public/signIn/page.js
+++ front-end-falk/app/(pages)/public/signIn/page.js
@@ -14,7 +14,7 @@ function SignIn() {
 
   return (
     <div className='parent flex items-center justify-center h-full w-full '>
-      <div className='child font-bold text-[29px] h-1/4 w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 flex  flex-col gap-4'>
+      <div className='child font-bold text-[29px]  w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 flex  flex-col gap-4 border-2 border-purple-700 p-4 rounded'>
         <div className='flex justify-center text-lg sm:text-base md:text-lg lg:text-xl xl:text-2xl'>Faça o login</div>
         <Input upText="Email" onChange={(e) => setEmail(e.target.value)}></Input>
         <Input upText="Senha" onChange={(e) => setPassword(e.target.value)} ></Input>
--- front-end-falk/app/(pages)/public/signUp/page.js
+++ front-end-falk/app/(pages)/public/signUp/page.js
@@ -15,7 +15,7 @@ function SignIn() {
 
   return (
     <div className="parent flex items-center justify-center h-screen w-screen ">
-      <div className="child font-bold text-[29px] h-1/4 w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 flex flex-col">
+      <div className="child font-bold text-[29px] h-1/4 w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 flex flex-col  border-2 border-purple-700 p-4 rounded">
         <div className="flex justify-center text-lg sm:text-base md:text-lg lg:text-xl xl:text-2xl">
           Cadastre-se
         </div>
