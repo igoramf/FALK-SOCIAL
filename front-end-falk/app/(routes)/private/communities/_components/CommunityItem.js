0baf0746637e03ed0f6811933aaa09e8fd77a42c
igoramf <igoraaf1@gmail.com>
2024-03-09 09:59:47 -0300

    fix: community image item dimensions

--- front-end-falk/app/(routes)/private/communities/_components/CommunityItem.js
+++ front-end-falk/app/(routes)/private/communities/_components/CommunityItem.js
@@ -14,11 +14,12 @@ function CommunityItem( { content } ) {
     router.push(`communities/${content.communityName}`)
   }
 
+  const profileImg = content?.profile_pic ? content?.profile_pic : userImg.src
+
   return (
     <div className='p-5 border rounded-lg my-5 hover:cursor-pointer' onClick={routerToDetails}>
         <div className='flex gap-2 items-center'>
-            <Image src={userImg} alt='user-image' width={35} height={35} 
-            className='rounded-full'/>
+            <img src={profileImg} alt='user-image' width={35} height={35} className="rounded-full"  style={{ maxWidth: '100%', maxHeight: '100%' }}/>
             <div>
                 <h2 className='font-bold'>{content.communityName}</h2>
                 <h2 className='text-[14px] text-gray-400'>{content.description}</h2>
